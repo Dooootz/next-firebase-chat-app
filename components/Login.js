1003e7020c18e38f967ab806c6ba17e69ec417b9
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-20 20:45:40 +1000

    Style components using ChakraUI

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import { ChatIcon } from '@chakra-ui/icons'
+import { Box, Button, Center, Stack } from "@chakra-ui/react"
 
 
 const Login = () => {
@@ -10,7 +11,30 @@ const Login = () => {
             <title>Login</title>
         </Head>
 
-        <ChatIcon w="100px" h="100px" color="black"/>
+        <Center 
+            height="100vh"
+        >
+            <Stack
+                align="center"
+                bgColor="grey"
+                p={16}
+                rounded="3xl"
+            >
+                <Box 
+                    bgColor="blue"
+                    w="fit-content"
+                    p={5}
+                    rounded="3xl"
+                    boxShadow="md"
+                >
+                    <ChatIcon w="100px" h="100px" color="white"/>
+                </Box>
+
+                <Button boxShadow="md">Sign In with Google</Button>
+            </Stack>
+
+        </Center>
+
     </>
 
   )
