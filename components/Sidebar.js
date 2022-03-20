58409b53a1484c380c05bf0e39621fc7a38a4af0
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-20 20:51:29 +1000

    Initialize Sidebar component

---
+++ components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Flex } from '@chakra-ui/react'
+
+const Sidebar = () => {
+  return (
+    <Flex
+        bg="blue.100"
+    >
+
+    </Flex>
+  )
+}
+
+export default Sidebar
