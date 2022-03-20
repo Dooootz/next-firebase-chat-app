dedb51bddb87fcc2e9a874c47ca449bf9a7bbc48
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-20 21:10:12 +1000

    Scrollbar layout & positioning v1 complete

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,23 +1,58 @@
 import React from 'react'
-import { Flex, IconButton, Text, Avatar } from '@chakra-ui/react' 
+import { Flex, IconButton, Text, Avatar, Button } from '@chakra-ui/react' 
 import { ArrowLeftIcon } from '@chakra-ui/icons'
+
+const Chat = () => {
+    return (
+        <Flex p={3} align="center" _hover={{bg: "gray.100", cursor: "pointer"}}>
+            <Avatar src="" marginEnd={3}/>
+            <Text>user@gmailcom</Text>
+        </Flex>
+    )
+}
 
 const Sidebar = () => {
   return (
     <Flex
-        bg="blue.100"
+        // bg="blue.100"
         w="300px" h="100vh"
         borderEnd="1px solid" borderColor="grey.200"
+        direction="column"
     >
         <Flex
-            bgColor="red.100"
+            // bgColor="red.100"
             h="81px" w="100%"
             align="center" justifyContent="space-between"
+            borderBottom="1px solid" borderColor="grey.200"
             p={3}
         >
-            <Avatar src="" />
-            <Text>Tre Molony</Text>
+            <Flex align="center">
+                <Avatar src="" marginEnd={3}/>
+                <Text>Tre Molony</Text>
+            </Flex>
+
             <IconButton size="sm" isRound icon={<ArrowLeftIcon/>} />
+
+        </Flex>
+
+        <Button m={5} p={4} >New Chat</Button>
+
+        <Flex overflowX="scroll" direction="column" sx={{scrollbarWidth: "none"}}>
+            <Chat/>
+            <Chat/>
+            <Chat/>
+            <Chat/>
+            <Chat/>
+            <Chat/>
+            <Chat/>
+            <Chat/>
+            <Chat/>
+            <Chat/>
+            <Chat/>
+            <Chat/>
+            <Chat/>
+            <Chat/>
+            <Chat/>
         </Flex>
 
     </Flex>
