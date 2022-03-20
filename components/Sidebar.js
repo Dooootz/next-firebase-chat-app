134776229d00e3ef719a2384a442f81350b62e3e
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-20 20:57:58 +1000

    Sidebar layout positioning

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import { Flex } from '@chakra-ui/react'
+import { Flex, IconButton, Text, Avatar } from '@chakra-ui/react' 
+import { ArrowLeftIcon } from '@chakra-ui/icons'
 
 const Sidebar = () => {
   return (
     <Flex
         bg="blue.100"
+        w="300px" h="100vh"
+        borderEnd="1px solid" borderColor="grey.200"
     >
+        <Flex
+            bgColor="red.100"
+            h="81px" w="100%"
+            align="center" justifyContent="space-between"
+            p={3}
+        >
+            <Avatar src="" />
+            <Text>Tre Molony</Text>
+            <IconButton size="sm" isRound icon={<ArrowLeftIcon/>} />
+        </Flex>
 
     </Flex>
   )
