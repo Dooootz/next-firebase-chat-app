19826958148298f0a3e43766d4fe7681aa1d1d5e
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-22 09:30:59 +1000

    Import firestore db

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
-// Your web app's Firebase configuration
+import { getFirestore } from 'firebase/firestore'
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBnhvO0B1aplLaWMIfbSE2_MSk5Xdf102U",
   authDomain: "chatapp-ea415.firebaseapp.com",
@@ -15,5 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
+const db = getFirestore(); 
 
 export { auth }
