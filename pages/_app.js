7a1ccf68578a4fd33f6922a6a3a1bf363cc41602
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-21 21:34:36 +1000

    Import global styles

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { ChakraProvider } from "@chakra-ui/provider"
 import Login from "../components/Login"
 import Sidebar from "../components/Sidebar"
 import Chat from"./chat/[id]"
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   // return <Component {...pageProps} />
