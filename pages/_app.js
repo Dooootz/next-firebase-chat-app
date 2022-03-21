bf9c394d20a49b8d438f90b5a4ef7a6c722ff8a3
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-21 22:34:57 +1000

    Authentication logic implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,33 @@
 import { ChakraProvider } from "@chakra-ui/provider"
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../firebaseconfig'
+
 import Login from "../components/Login"
 import Sidebar from "../components/Sidebar"
 import Chat from"./chat/[id]"
+
 import '../styles/globals.css'
+import { Spinner, Center } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-  // return <Component {...pageProps} />
+  const [user, loading, error] = useAuthState(auth);
+
+  if(loading) {
+    return (
+    <ChakraProvider>
+      <Center h="100vh">
+        <Spinner size="xl" />
+      </Center>
+    </ChakraProvider>
+    )
+
+  }
+
+  if(!user) {
+    <ChakraProvider>
+      <Login />
+    </ChakraProvider>
+  }
 
   return (
     <ChakraProvider>
