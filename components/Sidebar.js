6fe30f7563cebc8a560f67051c77d03498948c2d
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-24 23:21:29 +1000

    Fix indentation

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -34,91 +34,92 @@ import { useRouter } from 'next/router'
 // Sidebar functional component
 const Sidebar = () => {
 
-  // destructure user from useAuthState hook
-  // takes authentication as a parameter / handle
-  const [user] = useAuthState(auth);
+    // destructure user from useAuthState hook
+    // takes authentication as a parameter / handle
+    const [user] = useAuthState(auth);
 
-  // destructure useCollection hook - see docs for additional information about using this hook + parameters 
-  const [snapshot, loading, error] = useCollection(collection(getFirestore(db), "chats"));
+    // destructure useCollection hook - see docs for additional information about using this hook + parameters 
+    const [snapshot, loading, error] = useCollection(collection(getFirestore(db), "chats"));
 
-  // chats takes a snapshot of the live document 
-  // finds the document credentials that match of firebase config file 
-  const chats = snapshot?.docs.map((doc => ({id: doc.id, ...doc.data()})));
+    // chats takes a snapshot of the live document 
+    // finds the document credentials that match of firebase config file 
+    const chats = snapshot?.docs.map((doc => ({id: doc.id, ...doc.data()})));
 
-  // use next router function
-  const router = useRouter()  
+    // use next router function
+    const router = useRouter()  
 
-  // redirect callback function that takes uuid as a parameter 
-  const redirect = (id) => {
+    // redirect callback function that takes uuid as a parameter 
+    const redirect = (id) => {
     // router.push() function takes us to this api endpoint - where uuid === id
     router.push(`/chat/${id}`);
-  }
+    }
 
-  // single line callback function takes autheticated email as a parameter
-  // opens the chat document then loops thru to match user email with our auth email
-  const chatExists = email => chats?.find(chat => (chat.users.includes(user.email) && chat.users.includes(email)))
+    // single line callback function takes autheticated email as a parameter
+    // opens the chat document then loops thru to match user email with our auth email
+    const chatExists = email => chats?.find(chat => (chat.users.includes(user.email) && chat.users.includes(email)))
 
-  // asynchronous callback function 
-  // prompts user to enter the email of intended chat user
-  const newChat = async () => {
-      const input = prompt("Enter email of chat recipient");
-      // if chat with selected user does NOT exist
-      // await
-      // then create new chat with that user 
-      // if chat does exist... then do nothing
-      if(!chatExists(input) && (input != user.email)) {
-          await addDoc(collection((getFirestore(db)), "chats"), { users: [user.email, input]} );
-      }
-  }
-  
-  // filter thru all the distinct chats in firestore 
-  const chatList = () => {
-    return (
-        // match & validate the emails between both parties
+    // asynchronous callback function 
+    // prompts user to enter the email of intended chat user
+    const newChat = async () => {
+        const input = prompt("Enter email of chat recipient");
+        // if chat with selected user does NOT exist
+        // await
+        // then create new chat with that user 
+        // if chat does exist... then do nothing
+        if(!chatExists(input) && (input != user.email)) {
+            await addDoc(collection((getFirestore(db)), "chats"), { users: [user.email, input]} );
+        }
+    }
+
+    // filter thru all the distinct chats in firestore 
+    const chatList = () => {
+        return (
+            // match & validate the emails between both parties
         chats?.filter(chat => chat.users.includes(user.email))
-        .map(
+            .map(
             // upon matching, add this new element to the DOM
             chat =>
-            // onclick function that redirects user the shared chat session
-            <Flex key={Math.random()} p={3} align="center" _hover={{bg: "gray.100", cursor: "pointer"}} onClick={() => redirect(chat.id)}>
-                <Avatar src="" marginEnd={3}/>
-                {/* match & display that users email address */}
-                <Text>{getOtherEmail(chat.users, user)}</Text>
-            </Flex>
+                // onclick function that redirects user the shared chat session
+                <Flex key={Math.random()} p={3} align="center" _hover={{bg: "gray.100", cursor: "pointer"}} onClick={() => redirect(chat.id)}>
+                    <Avatar src="" marginEnd={3}/>
+                    {/* match & display that users email address */}
+                    <Text>{getOtherEmail(chat.users, user)}</Text>
+                </Flex>
+            )
         )
-    )
-}
-  return (
-    <Flex
+    }
+
+    return (
+        <Flex
         // bg="blue.100"
         w="300px" h="100vh"
         borderEnd="1px solid" borderColor="grey.200"
         direction="column"
-    >
-        <Flex
+        >
+            <Flex
             // bgColor="red.100"
             h="81px" w="100%"
             align="center" justifyContent="space-between"
             borderBottom="1px solid" borderColor="grey.200"
             p={3}
-        >
-            <Flex align="center">
-                <Avatar src={user.photoURL} marginEnd={3}/>
-                <Text>{user.displayName}</Text>
+            >
+                <Flex align="center">
+                    <Avatar src={user.photoURL} marginEnd={3}/>
+                    <Text>{user.displayName}</Text>
+                </Flex>
+
+                <IconButton onClick={() => signOut(auth)} size="sm" isRound icon={<ArrowLeftIcon/>} />
+
             </Flex>
 
-            <IconButton onClick={() => signOut(auth)} size="sm" isRound icon={<ArrowLeftIcon/>} />
+            <Button m={5} p={4} onClick={() =>newChat()} >New Chat</Button>
 
-        </Flex>
-
-        <Button m={5} p={4} onClick={() =>newChat()} >New Chat</Button>
-
-        <Flex overflowX="scroll" direction="column" flex={1}>
+            <Flex overflowX="scroll" direction="column" flex={1}>
             {chatList()}
-        </Flex>
+            </Flex>
 
-    </Flex>
-  )
+        </Flex>
+    )
 }
 
 export default Sidebar
