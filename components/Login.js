0a74b61c28b2218a47762ada865aee4dc07b9c8d
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-24 22:23:18 +1000

    imports explained

--- components/Login.js
+++ components/Login.js
@@ -1,8 +1,23 @@
+// == Login component ==
 import React from 'react'
+
+// import Head component from index.js in the root of pages folder
+// Exact same as a head tag in a html document 
+// When developing with Next.js, Head components must be imported from next.head
 import Head from 'next/head'
+
+// import chakra-ui icon library
 import { ChatIcon } from '@chakra-ui/icons'
+
+// import styling components from chakra-ui/react
+// useful additional info can be found by hovering over each component
 import { Box, Button, Center, Stack } from "@chakra-ui/react"
+
+// import react-firebase hooks / functions
+// useful info can be found in website & github docs
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
+
+// import the get auth function which is set up in our config file
 import { auth } from '../firebaseconfig'
 
 
